2fac4680be0d2cf3356a2185ae89821107b2a4ef
Feras Hamam <feras.i.hamam@gmail.com>
2022-02-26 00:06:01 +0300

    added customer entity, custoreDAO,controller, css for list customers html page and read from properties file

---
+++ src/main/java/com/spring/dao/CustomerDAOImpl.java
@@ -0,0 +1,28 @@
+package com.spring.dao;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.spring.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO{
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		Session session = sessionFactory.getCurrentSession();
+		List<Customer> customers = session.createQuery("from Customer",Customer.class).getResultList();
+		return customers;
+	}
+}
